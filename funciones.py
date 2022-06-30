8ac384f3e9895df423370bf0cca6cdb269af761c
Maria Lucia Solano Meneses <mariasolanomeneses@gmail.com>
2022-06-29 20:40:23 -0500

    Mi tercera funcion 29-07-2022

--- funciones.py
+++ funciones.py
@@ -5,5 +5,12 @@ def nombre(): # mi primer funcion
     a = 3
     b = 4
     c = a + b
-    print(c)
+    print("El resultado de la suma es: ", c)
 nombre()
+
+def res ():
+    a = 2
+    b = 1
+    c = a - b
+    print("El resultado de la resta es: ", c)
+res ()
