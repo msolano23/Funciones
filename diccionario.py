41169672fa9bbfaec104f2bbd2bdbc4ec4f65083
Maria Lucia Solano Meneses <mariasolanomeneses@gmail.com>
2022-06-29 22:01:14 -0500

    Nuevo repo

---
+++ diccionario.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+paises = {"Colombia":"Bogotá","Perú":"Lima","Lista":[1, 3, 5, 7]}
+print("Se muestra lista de paises: ", paises)
+
+print(paises["Lista"][1])
+print(paises.keys())
